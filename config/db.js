d3c8bc8d1057527b4e3e789ea68343a10e3c4808
ShaguftaSamar <162470125+ShaguftaSamar@users.noreply.github.com>
2026-02-24 10:12:26 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+const sslConfig = process.env.DB_SSL_CA_CONTENT 
+  ? { ca: Buffer.from(process.env.DB_SSL_CA_CONTENT, 'utf8') }
+  : { rejectUnauthorized: false };
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    ca: process.env.DB_SSL_CA_CONTENT,
-  },
+  ssl: sslConfig,
   waitForConnections: true,
   connectionLimit: 10,
 });
